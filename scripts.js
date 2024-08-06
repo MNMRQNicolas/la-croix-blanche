c60cceca6c0d44bdd43b2ede883df19d3c5de9f7
MNMRQNicolas <nicolas.monmerque@gmail.com>
2024-08-06 10:34:58 +0200

    Création du footer

--- scripts.js
+++ scripts.js
@@ -1,10 +1,16 @@
-// Charger la navigation
-function loadContent () {
-  fetch('nav.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('navigation').innerHTML = data;
-    })
-    .catch(error => console.error('Erreur chargement du footer :', error));
+// Chargement des contenus sur plusieurs pages navigation
+function loadContent() {
+  // Promesses pour charger la navigation et le footer
+  const navigationPromise = fetch('nav.html').then(response => response.text());
+  const footerPromise = fetch('footer.html').then(response => response.text());
+
+  // Utilisation de Promise.all pour attendre les deux fetch
+  Promise.all([navigationPromise, footerPromise])
+      .then(([navigationData, footerData]) => {
+          document.getElementById('navigation').innerHTML = navigationData;
+          document.getElementById('footer').innerHTML = footerData;
+      })
+      .catch(error => console.error('Error loading content:', error));
 }
+
 document.addEventListener('DOMContentLoaded', loadContent);
